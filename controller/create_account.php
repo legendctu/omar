cfbcf0f24c15ec343da7a94aa96f9b59f7d8ac66
Jieming.Liang <legendctu@gmail.com>
2012-06-19 14:34:43 +0800

    添加遗留文件

--- controller/create_account.php
+++ controller/create_account.php
@@ -1,44 +1,19 @@
 <?php
+include_once("api_helper.php");
+
 $email = isset($_POST["email"]) ? $_POST["email"] : "";
 $firstname = isset($_POST["firstname"]) ? $_POST["firstname"] : "";
 $lastname = isset($_POST["lastname"]) ? $_POST["lastname"] : "";
 $role = isset($_POST["role"]) ? $_POST["role"] : "";
 
-$fields = array( 
-    "email" => urlencode($email),
-    "firstname" => urlencode($firstname),
-    "lastname" => urlencode($lastname),
-    "role" => urlencode($role)
+$data = array( 
+    "email" => $email,
+    "firstname" => $firstname,
+    "lastname" => $lastname,
+    "role" => $role
 );
 
-$fields_string = "";
-foreach($fields as $key=>$value)
-{
-    $fields_string .= $key.'='.$value.'&' ;
-}
-rtrim($fields_string ,'&');
+echo json_encode(callapi("admin/account/create", "POST", $data));
 
 
-$headers["Content-Type"] = "application/json";
-$headers["Authorization"] = "Basic " . base64_encode($_COOKIE["OH_user"] . ":" . $_COOKIE["OH_pwd"]);
-
-$headerArr = array();
-foreach( $headers as $n => $v ) {
-  $headerArr[] = $n .':' . $v;
-}
- 
-ob_start();
-$ch = curl_init();
-curl_setopt ($ch, CURLOPT_URL, "http://coconut.yach.me/admin/account/create");
-curl_setopt ($ch, CURLOPT_HTTPHEADER , $headerArr);
-curl_setopt( $ch, CURLOPT_HEADER, 1);
-
-curl_setopt($ch, CURLOPT_POST,count($fields)) ;
-curl_setopt($ch, CURLOPT_POSTFIELDS,$fields_string) ;
- 
-curl_exec($ch);
-curl_close ($ch);
-$out = ob_get_contents();
-ob_clean();
-echo $out;
 ?>
