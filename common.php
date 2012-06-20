14c24eff48870a9d0579a65bb730771aa8e0a8bf
Li Donghua <getix2009@gmail.com>
2012-06-20 09:56:42 +0800

    Add common.php and modify stylesheets for main page.

--- common.php
+++ common.php
@@ -1,3 +1,6 @@
 <?php
-define("IMAGE_DIR", "/image/");
+define("ROOT_DIR", 'http://'.$_SERVER['HTTP_HOST'].'/omar/');
+define("CSS_DIR", ROOT_DIR."css/");
+define("IMG_DIR", ROOT_DIR."image/");
+define("JS_DIR", ROOT_DIR."script/");
 ?>
