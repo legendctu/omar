f999248ee21b9e79df298bd8bc75d42efab8abc3
Li Donghua <getix2009@gmail.com>
2012-06-20 14:37:34 +0800

    Remove header.php

--- view/view_helper.php
+++ view/view_helper.php
@@ -1,4 +1,30 @@
 <?php
+
+function render_header($title) {
+?>
+<html>
+    <head>
+		<title><?= $title ?></title>
+		<meta http-equiv="Content-Type"content="text/html; charset=utf-8" />
+		<link style="text/css" href="../css/base.css" rel="stylesheet"/>
+		<script type="text/javascript" src="../script/jquery-1.7.2.min.js"></script>
+    </head>
+    <body>
+		<div class="bg-white">
+		<div class="wrap">
+			<div id="welcome" class="fr font18 pr10 arial light-red">
+				Welcome <a href="#" class="blue b">#username</a>, 
+				<a href="#" class="arial carmine b">logout</a>
+			</div>
+			<div class="p20">
+				<img src="../image/omarhub-logo.png" width="96" class="mb-15"/>
+				<span id="omar" class="georgia"><b>Omar</b></span>
+				<span id="hub" class="georgia"><b>Hub</b></span>
+			</div>
+		</div>
+		</div><?php
+}
+
 function render_nav($current) {
 	$nav = array(
 		'profile'         => array('main-navi', 'profile.php',         'gray', 'PROFILE'          ),
