ce1827175a9cd7e88a53a8f78af8336eebcf41e9
Li Donghua <getix2009@gmail.com>
2012-06-20 13:59:38 +0800

    Footer & header

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+		<div id="footer" class="bg-darkblue">
+			<div class="wrap">
+				<div class="center p5 blue font18">Copyright&copy;2012, <b>4.5</b> Team. All rights reserved.</div>
+			</div>
+		</div>
+    </body>
+</html>
