0076c245f76da3e914a5e0971dd270434218855d
Li Donghua <getix2009@gmail.com>
2012-06-21 14:00:32 +0800

    Add nav.

--- view/profile.php
+++ view/profile.php
@@ -1,7 +1,9 @@
 <?php
 	require_once("view_helper.php");
 	require_once("../controller/api_helper.php");
-	
+
+	$type = isset($_GET["type"]) ? $_GET["type"] : "activity";
+
 	render_header('Profile');
 	render_nav('profile');
 
@@ -19,6 +21,13 @@
 				<a id="update" class="fr mt10 button-bg white r14 arial font18 b shadow">Submit</a>
 			</div>
 		</div>
+		<p id="l_side_navi" class="pl20 font24 freshcolor">
+            <span class="center mr15 b"><a href="<?= $type == "activity" ? "#" : "profile.php" ?>" class="<?= $type == "activity" ? "carmine" : "light-red" ?>">Activity</a></span>
+            <span class="center">/</span>
+            <span class="center ml15 mr15 b"><a href="<?= $type == "information" ? "#" : "profile.php?type=information" ?>" class="<?= $type == "information" ? "carmine" : "light-red" ?>">Information</a></span>
+            <span class="center">/</span>
+            <span class="center ml15 mr15 b"><a href="<?= $type == "innovation" ? "#" : "profile.php?type=innovation" ?>" class="<?= $type == "innovation" ? "carmine" : "light-red" ?>">Innovation</a></span>
+        </p>
 	</div>
 	<div id="r_side" class="fr w250">
 	b
