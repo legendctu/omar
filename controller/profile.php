4dc953579bc3602fc6efb95a994c45be550d864a
Jieming.Liang <legendctu@gmail.com>
2012-06-25 15:02:26 +0800

    修改profile controller; 开发浮动信息显示

--- controller/profile.php
+++ controller/profile.php
@@ -1,23 +1,35 @@
 <?php
-$firstname = isset($_POST["firstname"]) ? $_POST["firstname"] : "";
-$lastname = isset($_POST["lastname"]) ? $_POST["lastname"] : "";
-$gender = isset($_POST["gender"]) ? $_POST["gender"] : "";
-$languages = isset($_POST["languages"]) ? $_POST["languages"] : "";
-$work_fields = isset($_POST["work_fields"]) ? $_POST["work_fields"] : "";
-$work_location = isset($_POST["work_location"]) ? $_POST["work_location"] : "";
-$target_population = isset($_POST["target_population"]) ? $_POST["target_population"] : "";
-
-$data = array(
-    "firstname" => $firstname,
-    "lastname" => $lastname,
-    "gender" => $gender,
-    "languages" => $languages,
-    "work_fields" => $work_fields,
-    "work_location" => $work_location,
-    "target_population" => $target_population
-);
-
 require_once("api_helper.php");
 
-echo json_encode(callapi("profile", "POST", $data));
+$type = isset($_POST["type"]) ? $_POST["type"] : "";
+
+if($type == "get"){
+    $uid = isset($_POST["uid"]) ? $_POST["uid"] : "";
+    $res = callapi("profile/{$uid}", "GET");
+    $d = array(
+        "code" => $res["code"],
+        "content" => json_decode($res["content"])
+    );
+    echo json_encode($d);
+}else{
+    $firstname = isset($_POST["firstname"]) ? $_POST["firstname"] : "";
+    $lastname = isset($_POST["lastname"]) ? $_POST["lastname"] : "";
+    $gender = isset($_POST["gender"]) ? $_POST["gender"] : "";
+    $languages = isset($_POST["languages"]) ? $_POST["languages"] : "";
+    $work_fields = isset($_POST["work_fields"]) ? $_POST["work_fields"] : "";
+    $work_location = isset($_POST["work_location"]) ? $_POST["work_location"] : "";
+    $target_population = isset($_POST["target_population"]) ? $_POST["target_population"] : "";
+    
+    $data = array(
+        "firstname" => $firstname,
+        "lastname" => $lastname,
+        "gender" => $gender,
+        "languages" => $languages,
+        "work_fields" => $work_fields,
+        "work_location" => $work_location,
+        "target_population" => $target_population
+    );
+    
+    echo json_encode(callapi("profile", "POST", $data));
+}
 ?>
