e69b2e70fd3a07b523d9f38a47a8d7d830c57c2e
Jieming.Liang <legendctu@gmail.com>
2012-06-26 09:44:41 +0800

    controller/follow.php 更改，添加unfollow功能

--- controller/follow.php
+++ controller/follow.php
@@ -1,9 +1,12 @@
 <?php
     require_once("api_helper.php");
 	
-    $id = isset($_GET["id"]) ? $_GET["id"] : "";
-
-    $ret = callapi("friendships/".$id, "PUT", array());
-	
+	$id = isset($_GET["id"]) ? $_GET["id"] : "";
+	$type = isset($_GET["type"]) ? $_GET["type"] : "";
+    if($type == "follow"){
+        $ret = callapi("friendships/".$id, "PUT");
+    }else{
+        $ret = callapi("friendships/".$id, "DELETE");
+    }
     echo json_encode($ret);
 ?>
